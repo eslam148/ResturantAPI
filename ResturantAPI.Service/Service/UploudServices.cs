fcf17783d53672a8468a1f942f3b8617574a967e
eslam148 <islam.18333388@compit.aun.edu.eg>
2025-04-28 11:30:34 +0300

    delete azure

--- ResturantAPI.Service/Service/UploudServices.cs
+++ ResturantAPI.Service/Service/UploudServices.cs
@@ -8,21 +8,21 @@ namespace ResturantAPI.Services.Service
     {
         public async Task<string> UploadImageToAzureAsync(IFormFile file)
         {
-            string connectionString = "DefaultEndpointsProtocol=https;AccountName=eslam123456789;AccountKey=BxCgqrKzR1m5GoMbFJNybJysiEeKHvg1YAI0Uz0eVnDQ+zd7yRQWVGI8AbkGyAZUVuVp14H6SKqX+AStcqf2Ng==;EndpointSuffix=core.windows.net";
-            string containerName = "eslam1";
+            //string connectionString = "DefaultEndpointsProtocol=https;AccountName=eslam123456789;AccountKey=BxCgqrKzR1m5GoMbFJNybJysiEeKHvg1YAI0Uz0eVnDQ+zd7yRQWVGI8AbkGyAZUVuVp14H6SKqX+AStcqf2Ng==;EndpointSuffix=core.windows.net";
+            //string containerName = "eslam1";
 
-            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
-            await container.CreateIfNotExistsAsync(PublicAccessType.Blob); // يتيح الوصول العام للصور
+            //BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+            //await container.CreateIfNotExistsAsync(PublicAccessType.Blob); // يتيح الوصول العام للصور
 
-            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-            BlobClient blobClient = container.GetBlobClient(fileName);
+            //string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            //BlobClient blobClient = container.GetBlobClient(fileName);
 
-            using (var stream = file.OpenReadStream())
-            {
-                await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = file.ContentType });
-            }
+            //using (var stream = file.OpenReadStream())
+            //{
+            //    await blobClient.UploadAsync(stream, new BlobHttpHeaders { ContentType = file.ContentType });
+            //}
 
-            return blobClient.Uri.ToString(); // الرابط المباشر للصورة
+            return "Hello"; // blobClient.Uri.ToString(); // الرابط المباشر للصورة
         }
     }
 }
