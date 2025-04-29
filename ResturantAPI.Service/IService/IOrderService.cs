039722d66ecebcd2bde520da49b0b1e50d7b0fea
sabreen01 <Sabreen.20377412@compit.aun.edu.eg>
2025-04-29 16:00:31 +0300

    allapis

--- ResturantAPI.Service/IService/IOrderService.cs
+++ ResturantAPI.Service/IService/IOrderService.cs
@@ -1,8 +1,6 @@
 ﻿using ResturantAPI.Services.Dtos;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ResturantAPI.Services.IService
